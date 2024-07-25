3263e89125460d8e793832f9255ae216b7fdf14d
khody <ekhodor8@gmail.com>
2024-07-25 15:13:08 -0700

    added 2nd migration

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model): #models.Model is supplied by django to us. 
     title = models.CharField(max_length=50)  # you get the value types from the models module. # charfield is used for small to large-sized strings. have to set 
     #Not for something that is multiple book pages
-    rating = models.IntegerField() # integer numbers. 
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)]) # integer numbers. 
+    author = models.CharField(null=True, max_length=100) # these two new fields, author and is_bestselling were added after we make initial migrations, this means we have to set default values for them,
+    is_bestselling = models.BooleanField(default=False) # the sql datatable doesnt allow for empty values, (null is okay even though not optimal) 
+    #we have to have some default if were adding on new attributes after making migrations. or we could say blank=True and allow blank values.
 
     def __str__(self): # overriding this function as it basically exists for every python class. it allows us to change how something can be output in the terminal.
         return f"{self.title} ({self.rating})"
