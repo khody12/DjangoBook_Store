d71103e963e802b976ca8e024b2ad8bc28c17c8b
khody <ekhodor8@gmail.com>
2024-07-26 20:12:48 -0700

    Added administrater account

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    
+    prepopulated_fields = {"slug": ("title",)} # both slug and title is a field name here. 
+    list_filter = ("author","rating")
+    #django knows what to do here because it understands that slug is a slug field, and it'll make a slug based off the tuple that is passed in.
+
+
+
+admin.site.register(Book,BookAdmin)
+
+
