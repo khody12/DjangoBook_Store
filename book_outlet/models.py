7087ce612e62943310ec4a662e6b5406d309a770
khody <ekhodor8@gmail.com>
2024-07-27 21:29:25 -0700

    added country model many-to-many. updated class meta, added admin page for country model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,17 +4,30 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Address(models.Model): #THIS WILL BE A ONE TO ONE RELATION.
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta: # this meta class changes how things are output within the admin page. 
+        verbose_name_plural = "Address Entries"
     
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    Address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)   # ForeignKey() is used for a one-to-many relation, while OneToOneField is obviously one to one.
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)   # ForeignKey() is used for a one-to-many relation, while OneToOneField is obviously one to one.
     #null=True will allow us to make the migrations. we will initially get an error because we don't have a default value and we have existing data that doesnt have an address tied to author.
     #null=True is basically allowing this field to be set to null. 
 
@@ -35,6 +48,8 @@ class Book(models.Model): #models.Model is supplied by django to us.
     is_bestselling = models.BooleanField(default=False) # the sql datatable doesnt allow for empty values, (null is okay even though not optimal) 
     #we have to have some default if were adding on new attributes after making migrations. or we could say blank=True and allow blank values.
     slug = models.SlugField(default="",null=False, db_index=True) # this will slugify whatever is passed in. 
+
+    published_countries = models.ManyToManyField(Country)
     
     #we can improve the performance of the find operation by making an index for some of our attributes. db_index = database index. Use this for attributes that you will very often need to fetch.
 
