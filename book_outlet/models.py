0f87097ad3319310218d609ae0b4924a96e2faf8
khody <ekhodor8@gmail.com>
2024-07-28 10:38:30 -0700

    added class meta to country model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,6 +8,9 @@ class Country(models.Model):
     name = models.CharField(max_length=80)
     code = models.CharField(max_length=2)
 
+    def __str__(self):
+        return f"{self.name}"
+
     class Meta:
         verbose_name_plural = "Countries"
 
